4a748100638113ff07e17634bfa444ca594a1c00
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-31 13:12:33 -0600

    Message hint is showing up on state

--- lib/components/UserGuess.jsx
+++ lib/components/UserGuess.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 export default class UserGuess extends Component {
+
   render() {
-    const { lastGuess } = this.props;
+    const { lastGuess, randomNumberGenerated, messageHint } = this.props;
+    // const { messageHint } = this.messageHint(lastGuess, randomNumberGenerated);
 
     return (
       <section className ='userGuessForm'>
         <p>
           Your last guess was... { lastGuess }.
+        </p>
+        <p>
+          { messageHint }
         </p>
       </section>
     );
