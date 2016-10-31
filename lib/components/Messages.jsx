05e98fccf42598d020bb2f7e35dc8e5764a938a1
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-31 15:03:59 -0600

    Outside range message works

--- lib/components/Messages.jsx
+++ lib/components/Messages.jsx
@@ -2,22 +2,38 @@ import React, { Component } from 'react';
 
 export default class UserGuess extends Component {
 
+  // toggleMessage(outsideRangeMessage, messageHint){
+  //   if( outsideRangeMessage.length > 0){
+  //     return outsideRangeMessage;
+  //   }else{
+  //     return messageHint;
+  //   }
+  // }
+
   render() {
-    const { lastGuess, messageHint, outsideRangeMessage } = this.props;
+    const { lastGuess, outsideRangeMessage, messageHint } = this.props;
+    // const toggleMessage = this.toggleMessage(outsideRangeMessage, messageHint);
 
     return (
       <section className ='userGuessForm'>
         <span className = 'lastGuess'>
           Your last guess was... { lastGuess }.
         </span>
+
         <span className = 'messageHint'>
           { messageHint }
         </span>
+
         <span>
           { outsideRangeMessage }
         </span>
 
-        
+
+        {/* <span>
+          { toggleMessage }
+        </span> */}
+
+
       </section>
     );
   }
